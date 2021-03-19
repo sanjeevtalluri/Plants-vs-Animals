7414e8d18832ed2b5071e85c704ed5027cf0f778
sanjeevtalluri <sanjeevtalluri@gmail.com>
2021-03-19 15:01:16 +0530

    Added level loader

---
+++ Plants-vs-Animals/Assets/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int currBuildIndex;
+    [SerializeField]
+    private float waitTime=4f;
+    void Start()
+    {
+        currBuildIndex = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(ILoadSNextScene());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator ILoadSNextScene()
+    {
+        yield return new WaitForSeconds(waitTime);
+        LoadNextScene();
+    }
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currBuildIndex + 1);
+    }
+}
