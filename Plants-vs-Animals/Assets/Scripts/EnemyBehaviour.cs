face0ba16535106fe0906d55ffffbe1cee809eca
sanjeevtalluri <sanjeevtalluri@gmail.com>
2021-03-22 19:28:45 +0530

    Implemented enemy behaviour

---
+++ Plants-vs-Animals/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+  
+     [SerializeField]
+     [Range(0,5f)]
+     private float speed=5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left*Time.deltaTime*speed);
+    }
+}
---
+++ Plants-vs-Animals/Assets/Scripts/EnemySpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject lizardPrefab;
+
+    [SerializeField]
+    private int minSpawnTime=1;
+
+    [SerializeField]
+    private int maxSpawnTime=5;
+    private bool spawn=true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(IEnemySpawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private IEnumerator IEnemySpawner(){
+        while(spawn){
+            Instantiate(lizardPrefab,transform.position,Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(minSpawnTime,maxSpawnTime));
+        }
+    }
+}
