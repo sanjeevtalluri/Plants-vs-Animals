d65a1a33ef0ec621aa400ad7ae742eb91deb1f82
sanjeevtalluri <sanjeevtalluri@gmail.com>
2021-03-24 18:11:28 +0530

    Implemented Defender Spawner

---
+++ Plants-vs-Animals/Assets/Scripts/DefenderSpawnManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cactusPefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+       // Debug.Log("clicked");
+       Vector2 clickedWorldPos=GetWorldPosition();
+       Vector2 clickedGridPos=GetGridPosition(clickedWorldPos);
+       SpawnDefender(clickedGridPos);
+    }
+    private void SpawnDefender(Vector2 clickedWorldPos)
+    {
+        Instantiate(cactusPefab,clickedWorldPos,Quaternion.identity);
+    }
+    private Vector2 GetWorldPosition()
+    {
+        Vector2 clickedScreenPos=new Vector2(Input.mousePosition.x,Input.mousePosition.y);
+        return Camera.main.ScreenToWorldPoint(clickedScreenPos);
+    }
+
+    private Vector2 GetGridPosition(Vector2 clickedWorldPos){
+        float x=Mathf.RoundToInt(clickedWorldPos.x);
+        float y=Mathf.RoundToInt(clickedWorldPos.y);
+        return new Vector2(x,y);
+    }
+}
