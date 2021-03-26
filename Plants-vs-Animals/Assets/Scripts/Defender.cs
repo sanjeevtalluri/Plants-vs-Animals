dec2ad1422e9cc78aca97f83526fc864a70fc96b
sanjeevtalluri <sanjeevtalluri@gmail.com>
2021-03-26 15:20:25 +0530

    Implemeted Trophy mechanism

--- Plants-vs-Animals/Assets/Scripts/Defender.cs
+++ Plants-vs-Animals/Assets/Scripts/Defender.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Defender : MonoBehaviour
 {
+    private StarManager starManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+       starManager=FindObjectOfType<StarManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void AddStars(int amount)
+    {
+        starManager.RewardStars(amount);
     }
 }
---
+++ Plants-vs-Animals/Assets/Scripts/StarManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarManager : MonoBehaviour
+
+{
+    [SerializeField]
+    private Text starText;
+    private int stars=100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateStarText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void UpdateStarText()
+    {
+        starText.text=stars.ToString();
+    }
+    public void UseStars(int amount)
+    {
+        if(amount<=stars)
+        {
+            stars-=amount;
+            UpdateStarText();
+        }
+    }
+    public void RewardStars(int amount)
+    {
+        stars+=amount;
+        UpdateStarText();
+    }
+}
